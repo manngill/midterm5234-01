a1f8fed8a9454e7319815301c4681a18e06c6729
manngill <manpreetgill1946@gmail.com>
2019-05-16 12:01:43 -0400

    snake test

--- src/Snake.java
+++ src/Snake.java
@@ -9,61 +9,36 @@ public class Snake {
 	private int length; // the length of the boa, in feet
 	private String favoriteFood;
 
-	
+
 	/**
+	 * 
+	 * Student Name: Manpreet Kaur
+	 * Student ID:738175
+	 * 
+	 * 
+	 * 
+	 * 
 	 * Creates a new Snake object.
 	 * @param name			the name of the snake
 	 * @param length		length of the snake
 	 * @param favoriteFood	what the snake likes to eat
 	 */
-	
 	public Snake(String name, int length, String favoriteFood) {
 		this.name = name;
 		this.length = length;
 		this.favoriteFood = favoriteFood;
 	}
 
-	
+
 	/**
 	 * Snakes are healthy if they love vegetables
 	 * @return true if snake is healthy
 	 */
 	public boolean isHealthy() {
-		return this.favoriteFood.equals("vegetables");
-		
-		
-		ArrayList<Integer> Snake;
-		
-	
-		@Before
-		public void setUp() throws Exception {
-			// set up the initial values of your test array
-			// Creates a new array with (5,10,12) as the contents
-			myTestArray = new ArrayList<Integer>(Arrays.asList(5,10,12));
-		}
-
-		
-		@After
-		public void tearDown() throws Exception {
-		}
-		
-		@Test
-		public void testIndexOf() {
-			
-			int index = Snake.indexOf(new Integer(5)); // should return 0
-			assertEquals(0, index);
-			
-			// TC2: indexOf == find the position of something that doesnt exit
-			int indexOfDoesNotExist = Snake.indexOf(new Integer(5));
-			assertEquals(-1, indexOfDoesNotExist);
-			
-		}
-		
-		
-		
+		return this.favoriteFood.equals("vegetable");
 	}
 
-	
+
 	/**
 	 * Checks if snake will fit in a cage 
 	 * @return true if snake length < cage length
@@ -73,5 +48,5 @@ public class Snake {
 	public boolean fitsInCage(int cageLength) {
 		return this.length < cageLength;
 	}
-}
+} 
 
--- src/SnakeTest.java
+++ src/SnakeTest.java
@@ -1,22 +1,108 @@
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
 import static org.junit.Assert.*;
 
-import org.junit.Before;
-import org.junit.Test;
+/**
+ * Student Name: Manpreet Kaur 
+ * Student Id:738175
+ */
 
 public class SnakeTest {
 
-	private Snake peter;
-	private Snake takis;
-	
-	@Before
-	public void setUp() throws Exception {
-		peter = new Snake("Peter S", 10, "coffee");
-		takis = new Snake("Takis Z", 80, "vegetables");
-	}
+    private Snake peter;
+    private Snake Takis;
 
-	@Test
-	public void test() {
-		fail("Not yet implemented");
-	}
+    public SnakeTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() {
+    }
+
+    @AfterClass
+    public static void tearDownClass() {
+    }
+
+    @Before
+    public void setUp() {
+        peter = new Snake("Peter",5, "Tim bits");
+        Takis = new Snake("Takis",10, "vegetables");
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of isHealthy method, of class Snake.
+     */
+    @Test
+    public void testIsHealthy() {
+        System.out.println("isHealthy");
+        boolean expResult = false;
+
+        boolean result = peter.isHealthy();
+        assertEquals(expResult,result);
+        System.out.println("peter is healthy " + result);
+
+
+       
+        expResult = true;
+        result = Takis.isHealthy();
+        assertEquals(expResult,result);
+        System.out.println("Takis is healthy " + result);
+
+    }
+
+    /**
+     * Test of fitsInCage method, of class Snake.
+     */
+    @Test
+    public void testFitsInCage() {
+        System.out.println("fits In Cage");
+
+        int cageLength = 8;
+        boolean expResult = true;
+
+       
+        boolean result = peter.fitsInCage(cageLength);
+        assertEquals(expResult, result);
+        System.out.println("Peter fits in cage  " + result);
+
+     
+        result = peter.fitsInCage(10);
+        assertEquals(false, result);
+        System.out.println("Peter fits in cage ? " + result);
+
+       
+
+        result = peter.fitsInCage(11);
+        assertEquals(true, result);
+        System.out.println("Peter fits in cage ? " + result);
+       
+
+
+       
+        cageLength = 70;
+        expResult = false;
+        result = Takis.fitsInCage(cageLength);
+        assertEquals(expResult, result);
+        System.out.println("Takis fits in cage " + result);
+
+       
+        result = Takis.fitsInCage(30);
+        assertEquals(true, result);
+        System.out.println("Takis fits in cage "+result);
+
+        
+
+        result = Takis.fitsInCage(120);
+        assertEquals(false, result);
+        System.out.println("Takis fits in cage " + result);
+
+    }
 
 }
